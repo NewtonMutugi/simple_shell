f5d9d934d69df2d54248d7d99d4dbb34a508f65c
root <root@babyboo>
2023-02-18 17:55:34 +0300

    Added prompt"

--- main.c
+++ main.c
@@ -2,15 +2,17 @@
 
 /**
  * main - program entry point
+ * @ac: command count
+ * @av: argumrnt vectors
  * Return: int
  */
 
 int main(int ac, char **av)
 {
-	while(1)
+	while (1)
 	{
 		printf("%s", prompt);
-		read = getline(&command, &len, stin);
+		read = getline(&command, &len, stdin);
 
 		if (read == -1)
 		{
