1f43535d9763c88b4cb90b831e33ab7795e2f86b
NewtonMutugi <newtonmut197@gmail.com>
2023-02-22 21:05:32 +0300

    main.h part 1

--- main.h
+++ main.h
@@ -3,20 +3,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-
-const char *prompt = "#cisfun$ ";
-char *command = NULL;
-size_t len = 0;
-ssize_t read;
-char **args;
-char *token;
-char *path;
-char *path2;
-int status;
-
-#endif /* MAIN_H */
+#include <stdlib.h>
+#include <string.h>
